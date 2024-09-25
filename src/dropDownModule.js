6f1b81a98213ba489e6361373c9f2fd42915f87a
Alex <alexvnep@gmail.com>
2024-09-25 09:33:47 +0200

    Add functionality to drop down menu button to show when clicked

---
+++ src/dropDownModule.js
@@ -0,0 +1,21 @@
+const dropDown = () => {
+  const dropdownBtn = document.querySelector(".dropdown-btn");
+
+  const dropdownMenuItems = document.querySelector(".dropdown-menu-items");
+
+  dropdownBtn.addEventListener("click", () => {
+    console.log("I worked");
+    if (dropdownMenuItems.hasAttribute("hidden")) {
+      console.log("hidden");
+      dropdownMenuItems.removeAttribute("hidden");
+    } else {
+      dropdownMenuItems.setAttribute("hidden", "false");
+    }
+  });
+
+  return { dropdownBtn, dropdownMenuItems };
+};
+
+export default dropDown;
+
+//add eventListener with Onclick of dropdownBtn the dropdown-menu will be triggered visible or hidden
